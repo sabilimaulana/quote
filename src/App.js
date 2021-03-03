42fe6da899fd3082eddbb3831b0a458caf884a61
sabilimaulana <63346492+sabilimaulana@users.noreply.github.com>
2021-03-03 04:35:25 -0800

    selesai HTML dan CSS

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import "./styles.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import * as Icons from "@fortawesome/fontawesome-free-solid";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+library.add(fab);
 
 export default function App() {
   return (
     <div className="App">
       <div id="quote-box">
-        <div id="text">quote</div>
-        <div id="author">author</div>
-        <button id="new-quote">new</button>
-        <a id="tweet-quote"></a>
+        <div id="text">
+          <FontAwesomeIcon icon={Icons.faQuoteLeft} /> You can’t fall if you
+          don’t climb. But there’s no joy in living your whole life on the
+          ground.
+        </div>
+        <div className="bottom">
+          <div id="author">-Unknown</div>
+          <a id="tweet-quote" href="#">
+            <FontAwesomeIcon
+              id="twitter"
+              color="white"
+              size="2x"
+              icon={["fab", "twitter"]}
+            />
+          </a>
+          <button id="new-quote">new quote</button>
+        </div>
+        <p id="credit">made with &#10084; by sabilimaulana</p>
       </div>
     </div>
   );
