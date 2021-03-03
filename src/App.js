3bb7e8bfe313f823f5b858cd170fb8f6d618a39e
sabilimaulana <63346492+sabilimaulana@users.noreply.github.com>
2021-03-03 02:33:07 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div id="quote-box">
+        <div id="text">quote</div>
+        <div id="author">author</div>
+        <button id="new-quote">new</button>
+        <a id="tweet-quote"></a>
+      </div>
+    </div>
+  );
+}
